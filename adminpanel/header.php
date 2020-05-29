b4c0e5f946459c37b0581dca28d26a2236a917b7
Frida Johansson <54510229+frdah@users.noreply.github.com>
2020-05-29 17:09:57 +0200

    Adminpanel

---
+++ adminpanel/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles/style.css" />
+    <title>Adminpanel</title>
+  </head>
+  <body>
+    <nav class="navigation">
+      <img src="../styles/images/logga.png" alt=""/>
+      <div class="navigation-links">
+        <div class="header_categories">
+          <a class="navigation-link" href="http://fridajohansson.codes/Webbshopp_VG/">» Till butiken</a>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-over-all">
+    <div class="side-container">
+    <a href="../adminpanel"><h1 class="adminpanel-link">Adminpanelen</h1></a>
+
+    <a class="show-btn" href="./categories.php">Kategorier</a>
+    <a class="show-btn" href="../adminpanel">Beställningar</a>
+    </div>
